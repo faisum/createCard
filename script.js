1d0ca8b1d61fb9ad6a2fbd9f1da8c82d90d9be52
Your name <your-email@example.com>
2025-04-17 10:47:40 +0530

    final

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-function views(num) {
-    if (num >= 1000000) {
-        return (num / 1000000).toFixed(1) + "M"
-
-    } else if (num >= 1000) {
-        return (num / 1000).toFixed(1) + "k"
-    }
-    else {
-        return num.toString();
-    }
-}
-
-
 function createCard(title, cName, views, monthsOld, duration, thumbnail) {
 
     let viewStr;
-    if (viewStr < 1000) {
-        viewStr = views
 
-    } else if (viewStr > 1000000) {
-        viewStr = views / 1000000 + "M";
+    if (views < 1000) {
+        viewStr = views.toString();
+    } else if (views >= 1000 && views < 1000000) {
+        viewStr = (views / 1000).toFixed(1) + "k"; // Optional: toFixed(1) for one decimal place
     } else {
-        viewStr = views / 1000 + "k";
+        viewStr = (views / 1000000).toFixed(1) + "M"; // Optional: toFixed(1) for one decimal place
     }
 
     let html = `<div class="card">
@@ -36,7 +23,7 @@ function createCard(title, cName, views, monthsOld, duration, thumbnail) {
     document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
 
 }
-createCard("Installing Vs Code & How websites Work | Sigma Web Development Course - Tutorial #1", "CodeWithHarry", 3000000, "3", "31.22", "https://th.bing.com/th/id/OIP.SY0FtFRGZWgSpY2KEi5axAHaE8?w=269&h=180&c=7&r=0&o=5&pid=1.7")
+ createCard("Installing Vs Code & How websites Work | Sigma Web Development Course - Tutorial #1", "CodeWithHarry", 3000000, "3", "31.22", "https://th.bing.com/th/id/OIP.SY0FtFRGZWgSpY2KEi5axAHaE8?w=269&h=180&c=7&r=0&o=5&pid=1.7")
 
 createCard("Installing Vs Code & How websites Work | Sigma Web Development Course - Tutorial #2", "CodeWithHarry", 34567, "3", "31.22", "https://th.bing.com/th/id/OIP.SY0FtFRGZWgSpY2KEi5axAHaE8?w=269&h=180&c=7&r=0&o=5&pid=1.7")
 
